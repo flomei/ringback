d14af12884cd8b30b49dc7163bedcb200d0b22a2
Florian Meinicke <florianmeinicke@Florian-Meinickes-MacBook-Pro.local>
2017-05-10 12:16:22 +0200

    initial commit

---
+++ ringback.js
@@ -0,0 +1,40 @@
+"use strict";
+
+class Ringback {
+    constructor() {
+        this.eventCallbacks = {};
+    }
+
+    subscribe(eventName, callback) {
+        let callbackArray;
+        if (!this.eventCallbacks.hasOwnProperty(eventName)) {
+            this.eventCallbacks[eventName] = [];
+        }
+        callbackArray = this.eventCallbacks[eventName];
+        callbackArray.push(callback);
+    }
+
+    unsubscribe(eventName, callback) {
+        let callbackArray, callbackIndex;
+        if (!this.eventCallbacks.hasOwnProperty(eventName)) {
+            return;
+        }
+        callbackArray = this.eventCallbacks[eventName];
+        while ((callbackIndex = callbackArray.indexOf(callback)) !== -1) {
+            callbackArray.splice(callbackIndex, 1);
+        }
+    }
+
+    publish(eventName, ...callbackArguments) {
+        if (!this.eventCallbacks.hasOwnProperty(eventName)) {
+            return;
+        }
+        this.eventCallbacks[eventName].forEach(callback => callback(...callbackArguments));
+    }
+
+    clearAll() {
+        this.eventCallbacks = {};
+    }
+}
+
+module.exports = Ringback;
