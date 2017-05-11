75079a6b1eb41bf45157199d3b522bfecc204738
Florian Meinicke <florianmeinicke@Florian-Meinickes-MacBook-Pro.local>
2017-05-12 00:43:33 +0200

    Replace let with const

--- ringback.js
+++ ringback.js
@@ -9,23 +9,22 @@ class Ringback {
         if (typeof eventName !== 'string' || typeof callback !== 'function') {
             return;
         }
-        let callbackArray;
         if (!this.eventCallbacks.hasOwnProperty(eventName)) {
             this.eventCallbacks[eventName] = [];
         }
-        if (preventMultipleSubscriptions && this.eventCallbacks[eventName].includes(callback)) {
+        const callbackArray = this.eventCallbacks[eventName];
+        if (preventMultipleSubscriptions && callbackArray.includes(callback)) {
             return;
         }
-        callbackArray = this.eventCallbacks[eventName];
         callbackArray.push(callback);
     }
 
     unsubscribe(eventName, callback) {
-        let callbackArray, callbackIndex;
+        let callbackIndex;
         if (!this.eventCallbacks.hasOwnProperty(eventName)) {
             return;
         }
-        callbackArray = this.eventCallbacks[eventName];
+        const callbackArray = this.eventCallbacks[eventName];
         while ((callbackIndex = callbackArray.indexOf(callback)) !== -1) {
             callbackArray.splice(callbackIndex, 1);
         }
