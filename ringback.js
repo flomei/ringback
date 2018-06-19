ce413ce52bae12a7eca40724ee372477ac8d1847
florianmeinicke <contact@florianmeinicke.com>
2018-06-19 19:19:49 +0200

    Update formatting

--- ringback.js
+++ ringback.js
@@ -1,57 +1,57 @@
-"use strict";
+'use strict';
 
 class Ringback {
-    constructor() {
-        this.eventCallbacks = {};
-    }
+  constructor() {
+    this.eventCallbacks = {};
+  }
 
-    subscribe(eventName, callback, preventMultipleSubscriptions = true) {
-        if (typeof eventName !== 'string') {
-            throw 'eventName needs to be a string';
-        }
-        if (typeof callback !== 'function') {
-            throw 'callback needs to be a function';
-        }
-        if (!this.eventCallbacks.hasOwnProperty(eventName)) {
-            this.eventCallbacks[eventName] = [];
-        }
-        const callbackArray = this.eventCallbacks[eventName];
-        if (preventMultipleSubscriptions && callbackArray.includes(callback)) {
-            return;
-        }
-        callbackArray.push(callback);
+  subscribe(eventName, callback, preventMultipleSubscriptions = true) {
+    if (typeof eventName !== 'string') {
+      throw 'eventName needs to be a string';
     }
+    if (typeof callback !== 'function') {
+      throw 'callback needs to be a function';
+    }
+    if (!this.eventCallbacks.hasOwnProperty(eventName)) {
+      this.eventCallbacks[eventName] = [];
+    }
+    const callbackArray = this.eventCallbacks[eventName];
+    if (preventMultipleSubscriptions && callbackArray.includes(callback)) {
+      return;
+    }
+    callbackArray.push(callback);
+  }
 
-    unsubscribe(eventName, callback) {
-        if (typeof eventName !== 'string') {
-            throw 'eventName needs to be a string';
-        }
-        if (typeof callback !== 'function') {
-            throw 'callback needs to be a function';
-        }
-        let callbackIndex;
-        if (!this.eventCallbacks.hasOwnProperty(eventName)) {
-            return;
-        }
-        const callbackArray = this.eventCallbacks[eventName];
-        while ((callbackIndex = callbackArray.indexOf(callback)) !== -1) {
-            callbackArray.splice(callbackIndex, 1);
-        }
+  unsubscribe(eventName, callback) {
+    if (typeof eventName !== 'string') {
+      throw 'eventName needs to be a string';
     }
+    if (typeof callback !== 'function') {
+      throw 'callback needs to be a function';
+    }
+    let callbackIndex;
+    if (!this.eventCallbacks.hasOwnProperty(eventName)) {
+      return;
+    }
+    const callbackArray = this.eventCallbacks[eventName];
+    while ((callbackIndex = callbackArray.indexOf(callback)) !== -1) {
+      callbackArray.splice(callbackIndex, 1);
+    }
+  }
 
-    publish(eventName, ...callbackArguments) {
-        if (typeof eventName !== 'string') {
-            throw 'eventName needs to be a string';
-        }
-        if (!this.eventCallbacks.hasOwnProperty(eventName)) {
-            return;
-        }
-        this.eventCallbacks[eventName].forEach(callback => callback(...callbackArguments));
+  publish(eventName, ...callbackArguments) {
+    if (typeof eventName !== 'string') {
+      throw 'eventName needs to be a string';
     }
+    if (!this.eventCallbacks.hasOwnProperty(eventName)) {
+      return;
+    }
+    this.eventCallbacks[eventName].forEach(callback => callback(...callbackArguments));
+  }
 
-    clearAll() {
-        this.eventCallbacks = {};
-    }
+  clearAll() {
+    this.eventCallbacks = {};
+  }
 }
 
 module.exports = Ringback;
